5981578b26a7baa4deb99c7193f5a67c26e8cf61
James Lumsden <jameslumsden18@icloud.com>
2026-08-20 13:00:32 +0100

    chore: auto-commit 2026-08-20 13:00:32

--- main.py
+++ main.py
@@ -1619,6 +1619,7 @@ class JarvisLive:
         self._visual_watch_cfg = self._load_visual_watch_config()
         self._visitor_watch_cfg = self._load_visitor_watch_config()
         self._visitor_last_alert: dict[str, float] = {}
+        self._known_visitor_last_alert: dict[str, float] = {}
         self._visual_monitor = VisualMonitorRegistry(API_CONFIG_PATH)
         self._visual_error_seen: dict[str, tuple[str, float]] = {}
         self._visual_resync_last: float = 0.0
