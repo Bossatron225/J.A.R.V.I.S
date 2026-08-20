f25baa57e98d612c7969e5bc3b8430a6347ce13e
James Lumsden <jameslumsden18@icloud.com>
2026-08-20 17:10:27 +0100

    chore: auto-commit 2026-08-20 17:10:27

---
+++ ios/JarvisApp/JarvisApp/ContentView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+private enum AppStage {
+    case loggedOut
+    case needsBiometric
+    case ready
+}
+
+struct ContentView: View {
+    @State private var stage: AppStage = JarvisAPI.isLoggedIn ? .needsBiometric : .loggedOut
+
+    var body: some View {
+        Group {
+            switch stage {
+            case .loggedOut:
+                LoginView(onLoggedIn: { stage = .needsBiometric })
+            case .needsBiometric:
+                BiometricScanView(onUnlocked: { stage = .ready })
+            case .ready:
+                ChatView(
+                    onNeedsLogin: { stage = .loggedOut },
+                    onNeedsBiometric: { stage = .needsBiometric }
+                )
+            }
+        }
+        .animation(.default, value: stage == .ready)
+    }
+}
